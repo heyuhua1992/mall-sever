82aae36630f7f772130f39ec618660e7f251f97b
HeYuhua <heyuhua1992@163.com>
2018-09-06 14:02:05 +0800

    去除无意义部分

--- util/resJ.js
+++
@@ -1,9 +0,0 @@
-let resJ = (status, msg, result, res) => {
-  return res.json({
-    status: parseInt(status), // 默认 1出错， 0正常
-    msg: msg,
-    result: result ? result : ''
-  })
-}
-
-module.exports = resJ
